d89dac5f8b00435a222c5ee2c28ee6ef02f7686f
jxreyes <jxreyes@uw.edu>
2026-02-23 06:04:59 +0000

    complete week 7

---
+++ homework8_<jamie_reyes>.py
@@ -0,0 +1,11 @@
+pizza_orders = ["Cheese", "Pepperoni", "Hawaiian", "Veggie", "Supreme" ] 
+
+finished_pizzas = []
+
+for pizza in pizza_orders:
+    print("Your pizza pie is finished!")
+
+    finished_pizzas.append(pizza)
+
+for pizza in finished_pizzas:
+    print(f"The pizza {pizza} was made.")
